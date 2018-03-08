1137c287ba412f982694f9c7a4e1771c26ac4a4a
truezombie <oleg-enzo@yandex.ua>
2018-03-08 12:07:32 +0200

    Prod webpack config was changed.

--- configs/webpack/rules/scss.css.prod.js
+++ configs/webpack/rules/scss.css.prod.js
@@ -7,41 +7,39 @@ module.exports = function(paths) {
                 {
                     test: /\.scss$/,
                     exclude: /node_modules/,
-                    use: extract.extract({
-                        fallback: 'style-loader',
-                        use: [{ 
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1,
+                    use: [{
+                        loader: 'style-loader',
+                    }, { 
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        }
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.join(__dirname, '../../postcss_prod/postcss.config.js'),
                             }
-                        }, {
-                            loader: 'postcss-loader',
-                            options: {
-                                config: {
-                                    path: path.join(__dirname, '../../postcss_prod/postcss.config.js'),
-                                }
-                            } 
-                        }, { 
-                            loader: 'sass-loader',
-                        }]
-                    }),
+                        } 
+                    }, { 
+                        loader: 'sass-loader',
+                    }]
                 },
                 {
                     test: /\.css$/,
-                    exclude: /node_modules/,
-                    use: extract.extract({
-                        fallback: 'style-loader',
-                        use: [{
-                            loader: 'postcss-loader',
-                            options: {
-                                config: {
-                                    path: path.join(__dirname, '../../postcss_prod/postcss.config.js')
-                                }
+                    exclude: /node_modules/,      
+                    use: [{
+                        loader: 'style-loader',
+                    }, {
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: path.join(__dirname, '../../postcss_prod/postcss.config.js')
                             }
-                        }, {
-                            loader: 'css-loader',
-                        }]
-                    }),
+                        }
+                    }, {
+                        loader: 'css-loader',
+                    }]
                 },
             ],
         },
