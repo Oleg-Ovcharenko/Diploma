5e7983fc3e0e4b21714e7311b1ac5f7fc5844c96
Oleg Ovcharenko <oleg-enzo@yandex.ua>
2018-05-15 23:45:49 +0300

    refactoring settings

--- src/js/components/MainLayout/ControlsNetwork.js
+++ src/js/components/MainLayout/ControlsNetwork.js
@@ -23,32 +23,50 @@ class ControlsNetwork extends Component {
         } = this.props;
 
         return (
-            <div className="card mb-3 h-80 bg-white rounded-0">
-                <div className="card-body d-flex align-i-center rounded-0 d-flex justify-content-between">
-                    <div className="custom-control custom-checkbox">
-                        <input
-                            type="checkbox"
-                            className="custom-control-input custom-control-input-sm"
-                            id="customCheck1"
-                            disabled={nodes.length === 0}
-                            onClick={this.onShowRange}
-                        />
-                        <label
-                            className="custom-control-label small cursor-pointer"
-                            htmlFor="customCheck1"
-                        >
-                            Show range
-                        </label>
+            <div className="w-100 d-flex">
+                <div className="w-50 mr-2">
+                    <div className="card mb-3 h-80 bg-white rounded-0 w-100">
+                        <div className="card-body d-flex align-i-center rounded-0 d-flex justify-content-between">
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-success btn-sm"
+                                    onClick={this.onChangeAlgorithmStatus}
+                                    disabled={nodes.length === 0}
+                                >
+                                    Build algorithm
+                                </button>
+                            </div>
+                            <div className="custom-control custom-checkbox">
+                                <input
+                                    type="checkbox"
+                                    className="custom-control-input custom-control-input-sm"
+                                    id="customCheck1"
+                                    disabled={nodes.length === 0}
+                                    onClick={this.onShowRange}
+                                />
+                                <label
+                                    className="custom-control-label small cursor-pointer"
+                                    htmlFor="customCheck1"
+                                >
+                                    Show range
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <button
-                            type="submit"
-                            className="btn btn-success btn-sm"
-                            onClick={this.onChangeAlgorithmStatus}
-                            disabled={nodes.length === 0}
-                        >
-                            Build algorithm
-                        </button>
+                </div>
+                <div className="w-50">
+                    <div className="card mb-3 h-80 bg-white rounded-0 w-100">
+                        <div className="card-body d-flex align-i-center rounded-0 d-flex justify-content-between">
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-success btn-sm"
+                                >
+                                    Show results
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
