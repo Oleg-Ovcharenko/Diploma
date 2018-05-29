9132ad5a7b61a1179f24c8bcdcd6f729cc58ff9c
Oleg Ovcharenko <oleg-enzo@yandex.ua>
2018-05-30 00:12:10 +0300

    add favicon

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,13 +1,17 @@
 // LIBRARIES
 import React from 'react';
 import PropTypes from 'prop-types';
+import Favicon from 'react-favicon';
 // COMPONENTS
 import Header from './Header';
 import SideBar from './SideBar';
 import MainLayout from './MainLayout';
+// ICON
+import favicon from '../../img/favicon.png';
 
 const App = ({ dispatch }) => (
     <div className="app d-flex flex-column vh-height-100">
+        <Favicon url={favicon} />
         <Header />
         <main className="app-main d-flex flex-grow-1 min-height-500">
             <SideBar dispatch={dispatch} />
