f5d298a27b709b0e6f484248efab30a7ee66f710
Oleg Ovcharenko <oleg-enzo@yandex.ua>
2018-06-17 11:08:43 +0300

    refactoring

--- configs/webpack/rules/scss.css.dev.js
+++ configs/webpack/rules/scss.css.dev.js
@@ -1,7 +1,6 @@
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = function(paths) {
+module.exports = function (paths) {
     return {
         module: {
             rules: [
@@ -10,46 +9,46 @@ module.exports = function(paths) {
                     exclude: /node_modules/,
                     use: [
                         {
-                            loader: "style-loader",
+                            loader: 'style-loader',
                         }, {
                             loader: 'css-loader',
                             options: {
                                 sourceMap: true,
                                 importLoaders: 1,
-                            }
+                            },
                         }, {
                             loader: 'postcss-loader',
                             options: {
                                 sourceMap: true,
                                 config: {
                                     path: path.join(__dirname, '../../postcss_dev/postcss.config.js'),
-                                }
-                            } 
+                                },
+                            },
                         }, {
                             loader: 'sass-loader',
                             options: {
                                 sourceMap: true,
-                            }
-                        }
-                    ]
+                            },
+                        },
+                    ],
                 },
                 {
                     test: /\.css$/,
                     exclude: /node_modules/,
                     use: [
                         {
-                            loader: "style-loader",
+                            loader: 'style-loader',
                         },
                         {
                             loader: 'css-loader',
                             options: {
                                 sourceMap: true,
                                 importLoaders: 1,
-                            }
-                        }
-                    ]
-                }
-            ]
-        }
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
     };
 };
